f2412bd8ea865c52a8f58e5bb45c3f35b6f46722
David <adavidruizdiaz@gmail.com>
2019-04-07 15:13:00 -0400

    base

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Esta aplicación es un ejemplo de como consumir datos
+ * proveidos por una API REST en formato JSON.
+ *
+ * La API se encuentra en https://3a8i3.github.io/
+ * y devuelve un array de objetos con datos de 7 musicas
+ * 
+ * La aplicación debe recuperar los datos proveidos por la API
+ * y mostrarlos en la página.
+ *
+ * # PASOS A SEGUIR #
+ * 1. Recuperar la plantilla html en la que se mostrarán los datos y guardarla una variable
+ * 2. Recuperar los datos de la API utilizando AJAX a través de la función jquery.get()
+ * 3. Pasar los datos recuperados a una función que se encargará de cargar los datos
+ *    en la plantilla y mostrar en la página.
+ *    # PASOS DE LA FUNCION #
+ *    3.1. Crear un ciclo para recorrer el array de datos recuperado
+ *    3.2. Crear una variable que guarde el objeto jquery de la plantilla para cada ciclo
+ *    3.3. Recuperar y modificar los elementos de la plantilla correspondientes para cada
+ *         dato
+ *    3.4. Cargar la plantilla ya modificada en la página
+ *    
+ */
